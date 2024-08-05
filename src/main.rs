5cdc2bfe2528826d6f95927651d53442abd29f7e
HACBIT <107751081+hacbit@users.noreply.github.com>
2024-08-05 16:50:01 +0800

    美化输出格式，更新删除文件夹的实现

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::{Read, Write};
-use std::path::PathBuf;
+use std::fs::{self, File};
+use std::io::{self, Read, Write};
+use std::path::{Path, PathBuf};
 use zip::read::ZipArchive;
 use cfg_if::cfg_if;
 use sysinfo::System;
 use colored::Colorize;
+use chrono::Local;
 
 cfg_if!(
     if #[cfg(target_os = "macos")] {
@@ -21,30 +22,33 @@ cfg_if!(
     }
 );
 
-/// The default directory to extract the downloaded file
-const DEFAULT_DIR: &str = ".";
+macro_rules! local_time {
+    () => {
+        Local::now().format("%Y-%m-%d %H:%M:%S").to_string()
+    };
+}
 
 macro_rules! info { 
     ($($arg:tt)*) => {
-        println!("{} {}", "[INFO]".bright_cyan(), format!($($arg)*).bright_cyan());
+        println!("{} {}", format!("[INFO {}]", local_time!()).bright_cyan(), format!($($arg)*).bright_cyan());
     }
 }
 
 macro_rules! success {
     ($($arg:tt)*) => {
-        println!("{} {}", "[SUCCESS]".bright_green(), format!($($arg)*).bright_green());
+        println!("{} {}", format!("[SUCCESS {}]", local_time!()).bright_green(), format!($($arg)*).bright_green());
     };
 }
 
 macro_rules! warn {
     ($($arg:tt)*) => {
-        println!("{} {}", "[WARN]".bright_yellow(), format!($($arg)*).bright_yellow());
+        println!("{} {}", format!("[WARN {}]", local_time!()).bright_yellow(), format!($($arg)*).bright_yellow());
     };
 }
 
 macro_rules! error {
     ($($arg:tt)*) => {
-        eprintln!("{} {}", "[ERROR]".bright_red(), format!($($arg)*).bright_red());
+        eprintln!("{} {}", format!("[ERROR {}]", local_time!()).bright_red(), format!($($arg)*).bright_red());
     }
 }
 
@@ -77,7 +81,13 @@ fn main() -> Result<(), String> {
         }
         PathBuf::from(&args[2])
     } else {
-        PathBuf::from(DEFAULT_DIR)
+        match std::env::current_dir().map_err(|e| e.to_string()) {
+            Ok(dir) => dir,
+            Err(e) => {
+                error!("Failed to get the current directory: {}", e);
+                return end();
+            }
+        }
     };
 
     info!("Wait for Ani app to exit");
@@ -88,31 +98,56 @@ fn main() -> Result<(), String> {
 
     info!("Extracting {} to: {}", archive_path.display(), extract_path.display());
 
+    let try_remove = |path: &PathBuf| {
+        if path.exists() {
+            for _ in 0..5 {
+                if if path.is_file() { 
+                    std::fs::remove_file(path) 
+                } else { 
+                    debug_assert!(path.is_dir());
+
+                    // std::fs::remove_dir_all(path)
+                    remove_dir_filter(
+                        path, 
+                        std::env::current_exe()
+                            .ok()
+                            .as_ref()
+                            .map(|p| vec![p])
+                            .as_deref()
+                            .unwrap_or(&[])
+                    )
+                }.is_err() {
+                    info!("Wait for blocked files: {}", path.display());
+                    std::thread::sleep(std::time::Duration::from_secs(1));
+                } else {
+                    return Ok(());
+                }
+            }
+            Err(())
+        } else {
+            Ok(())
+        }
+    };
+
     // remove the old Ani app
     let app = extract_path.join(APP_NAME);
-    if app.exists() {
-        if let Err(e) = std::fs::remove_file(&app).map_err(|e| e.to_string()) {
-            error!("Failed to remove the old Ani app: {}", e);
-            return end();
-        }
+    if try_remove(&app).is_err() {
+        return end();
     }
-    if PathBuf::from("Ani.ico").exists() {
-        if let Err(e) = std::fs::remove_file("Ani.ico").map_err(|e| e.to_string()) {
-            error!("Failed to remove the old Ani icon: {}", e);
-            return end();
-        }
+
+    let ico = extract_path.join("Ani.ico");
+    if try_remove(&ico).is_err() {
+        return end();
     }
-    if PathBuf::from("app").exists() {
-        if let Err(e) = std::fs::remove_dir_all("app").map_err(|e| e.to_string()) {
-            error!("Failed to remove the old Ani app: {}", e);
-            return end();
-        }
+
+    let app_dir = extract_path.join("app");
+    if try_remove(&app_dir).is_err() {
+        return end();
     }
-    if PathBuf::from("runtime").exists() {
-        if let Err(e) = std::fs::remove_dir_all("runtime").map_err(|e| e.to_string()) {
-            error!("Failed to remove the old Ani runtime: {}", e);
-            return end();
-        }
+
+    let runtime_dir = extract_path.join("runtime");
+    if try_remove(&runtime_dir).is_err() {
+        return end();
     }
 
     success!("Old Ani app removed");
@@ -191,6 +226,25 @@ fn wait_app_exit() -> Result<(), String> {
 
         if ok {
             break;
+        }
+    }
+
+    Ok(())
+}
+
+fn remove_dir_filter(dir: impl AsRef<Path>, filter_paths: &[impl AsRef<Path>]) -> io::Result<()> {
+    let dir = dir.as_ref();
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let path = entry?.path();
+            if filter_paths.iter().any(|p| p.as_ref() == &path) {
+                continue;
+            }
+            if path.is_dir() {
+                remove_dir_filter(&path, filter_paths)?;
+            } else {
+                fs::remove_file(&path)?;
+            }
         }
     }
 
