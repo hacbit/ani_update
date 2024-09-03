71517fd70b35f604ec0a0eb848a7b6dad7f9d5d8
HACBIT <107751081+hacbit@users.noreply.github.com>
2024-09-04 01:14:58 +0800

    添加 ani_update_macro 依赖来优化 log api，并且修复了原来 {} 占位符没有被替换的 bug

--- src/lang.rs
+++ src/lang.rs
@@ -2,6 +2,7 @@
 //! the language of the program.
 
 use cfg_if::cfg_if;
+use ani_update_macro::invoke;
 
 pub enum Language {
     En,
@@ -38,53 +39,36 @@ impl Language {
     }
 }
 
-macro_rules! invoke {
-    ($($id:ident => $en:expr, $zh:expr),*$(,)?) => {
-        pub enum Lang {
-            $($id),*
-        }
 
-        impl Lang {
-            pub fn get(&self) -> &str {
-                match *self {
-                    $(Lang::$id => match Language::get() {
-                        Language::En => $en,
-                        Language::Zh => $zh,
-                    })*
-                }
-            }
-        }
+#[macro_export]
+macro_rules! generate_macro {
+    ($($id:ident => $en:expr, $zh:expr),*) => {
+        $(
+            invoke!($id => $en, $zh);
+        )*
     }
 }
 
-invoke! {
-    ErrorOccurred => "An error occurred", "发生了一个错误",
-    UpdaterStarted => "Ani app updater started", "Ani 程序更新器已启动",
-    Usage => "Usage: {} <archive> [extract_dir]", "用法: {} <压缩档案> [提取目录]",
-    TooManyArguments => "Too many arguments, ignore", "参数过多，忽略",
-    FailedToGetCurrentDir => "Failed to get the current directory", "无法获取当前目录",
-    WaitAniAppExit => "Wait for Ani app to exit", "等待 Ani 程序退出",
-    AniAppIsClosed => "Ani app is closed", "Ani 程序已关闭",
-    ExtractTo => "Extracting {} to {}", "正在提取 {} 到 {}",
-    ExtractFailed => "Failed to extract the archive due to {}", "无法提取压缩档案由于 {}",
-    Extracting => "Extracting {}", "正在提取 {}",
-    WaitBlock => "Wait for the blocked files to be released: {}", "等待被锁定的文件被释放: {}",
-    AniRemoved => "Ani app has been removed", "Ani 程序已被移除",
-    RemoveFailed => "Failed to remove the file", "无法移除文件",
-    CreateFileFailed => "Failed to create the file", "无法创建文件",
-    CreateDirFailed => "Failed to create the directory", "无法创建目录",
-    WriteFailed => "Failed to write the file", "无法写入文件",
-    UpdateCompleted => "Ani app update completed! :D", "Ani 程序更新完成! :D",
-    StartAniApp => "Wait for Ani app to start", "等待 Ani 程序启动",
-    StartAniFailed => "Failed to start the Ani app", "启动 Ani 程序失败",
-    End => "Press <Enter>/<Return> to exit...", "按 <Enter> 或 <Return> 键关闭窗口...",
-    PermissionDenied => "Permission denied, try to change the file permission", "访问被拒绝，尝试修改文件权限",
-}
-
-/// A helper macro to get the language string.
-#[macro_export]
-macro_rules! lang {
-    ($id:ident) => {
-        Lang::$id.get()
-    };
+generate_macro! {
+    error_occurred => "An error occurred with {}", "发生了一个错误由于 {}",
+    updater_started => "Ani app updater started", "Ani 程序更新器已启动",
+    usage => "Usage: {} <archive> [extract_dir]", "用法: {} <压缩档案> [提取目录]",
+    too_many_args => "Too many arguments, ignore {}", "参数过多，忽略 {}",
+    failed_get_current_dir => "Failed to get the current directory due to: {}", "无法获取当前目录由于: {}",
+    wait_app_exit => "Wait for Ani app to exit", "等待 Ani 程序退出",
+    ani_closed => "Ani app is closed", "Ani 程序已关闭",
+    extract_to => "Extracting {} to {}", "正在提取 {} 到 {}",
+    extract_failed => "Failed to extract the archive due to {}", "无法提取压缩档案由于 {}",
+    extracting => "Extracting {}", "正在提取 {}",
+    wait_blocked => "Wait for the blocked files to be released: {}", "等待被锁定的文件被释放: {}",
+    ani_removed => "Ani app has been removed", "Ani 程序已被移除",
+    remove_failed => "Failed to remove the file due to {}", "无法移除文件由于 {}",
+    create_file_failed => "Failed to create the file due to {}", "无法创建文件由于 {}",
+    create_dir_failed => "Failed to create the directory due to {}", "无法创建目录由于 {}",
+    write_failed => "Failed to write the file due to {}", "无法写入文件由于 {}",
+    update_finish => "Ani app update completed! :D", "Ani 程序更新完成! :D",
+    start_ani => "Wait for Ani app to start", "等待 Ani 程序启动",
+    start_ani_failed => "Failed to start the Ani app due to {}", "启动 Ani 程序失败由于 {}",
+    end => "Press <Enter>/<Return> to exit...", "按 <Enter> 或 <Return> 键关闭窗口...",
+    permission_denied => "Permission denied by {}, try to change the file permission", "访问被拒绝由于 {}，尝试修改文件权限"
 }
